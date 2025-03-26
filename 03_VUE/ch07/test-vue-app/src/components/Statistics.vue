70e7c198d05794ed7f283f990fca3c10b30262b2
hyungEee <jihyung772@gmail.com>
2025-03-26 13:05:57 +0900

    3/25 실습문제 풀이

--- 03_VUE/ch07/test-vue-app/src/components/Statistics.vue
+++ 03_VUE/ch07/test-vue-app/src/components/Statistics.vue
@@ -18,22 +18,75 @@
     <h2>📊 주문 통계</h2>
     <div class="statContents">
       <div>총 주문 횟수: {{ orders.length }}회</div>
-      <div>총 매출: {{ sum }}회</div>
-      <div>가장 인기있는 음료:</div>
-      <div>가장 많이 선택된 사이즈:</div>
-      <div>추가 샷 비율:</div>
+      <div>총 매출: {{ sales }}원</div>
+      <div>가장 인기있는 음료: {{ popularDrink }}</div>
+      <div>가장 많이 선택된 사이즈: {{ popularSize }}</div>
+      <div>추가 샷 비율: {{ extraShotRate }}%</div>
     </div>
   </div>
 </template>
 <script>
 export default {
   name: "Statistics",
-  props: ["orders"],
+  props: {
+    orders: {
+      type: Array,
+      default: () => [],
+    },
+  },
   data() {
     return {};
   },
   computed: {
-    sum() {},
+    sales() {
+      let sum = 0;
+      for (let i of this.orders) {
+        sum += i.price;
+      }
+      return sum;
+    },
+    popularDrink() {
+      const count = {};
+      this.orders.forEach((order) => {
+        count[order.drinkName] = (count[order.drinkName] || 0) + 1;
+      });
+
+      let maxName = "";
+      let maxCount = 0;
+
+      for (let [name, cnt] of Object.entries(count)) {
+        if (cnt > maxCount) {
+          maxName = name;
+          maxCount = cnt;
+        }
+      }
+      return `${maxName} (${maxCount}회)`;
+    },
+    popularSize() {
+      const count = {};
+      this.orders.forEach((order) => {
+        count[order.size] = (count[order.size] || 0) + 1;
+      });
+      let maxSize = "";
+      let maxCount = 0;
+      for (let [size, cnt] of Object.entries(count)) {
+        if (cnt > maxCount) {
+          maxSize = size;
+          maxCount = cnt;
+        }
+      }
+      return maxSize;
+    },
+    extraShotRate() {
+      let es = this.orders.reduce((acc, cur) => {
+        if (cur.extraShot) {
+          return (acc += 1);
+        } else {
+          return acc;
+        }
+      }, 0);
+      return (es / this.orders.length).toFixed(2) * 100;
+    },
   },
 };
 </script>
