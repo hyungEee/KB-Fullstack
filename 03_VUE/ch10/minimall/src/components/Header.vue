4242f4178f83d5f04d5d2d2e4ca93553c5103990
지형 <97657166+hyungEee@users.noreply.github.com>
2025-04-04 21:27:17 +0900

    Update Header.vue

--- 03_VUE/ch10/minimall/src/components/Header.vue
+++ 03_VUE/ch10/minimall/src/components/Header.vue
@@ -3,11 +3,7 @@ nav {
   margin: 0 auto;
   border-bottom: 1px solid gray;
 }
-ul {
-  padding: 20px 0 20px 0;
-  background-color: black;
-}
-.navi {
+.nav-ul {
   display: flex;
   flex-direction: row;
   justify-content: center;
@@ -15,8 +11,10 @@ ul {
   font-size: 1.4em;
   font-weight: bold;
   margin: 0px;
+  padding: 20px 0 20px 0;
+  background-color: black;
 }
-li {
+.nav-li {
   margin-left: 30px;
   margin-right: 30px;
   color: white;
@@ -24,14 +22,14 @@ li {
 </style>
 <template>
   <nav>
-    <ul class="navi">
-      <li>
+    <ul class="nav-ul">
+      <li class="nav-li">
         <router-link class="nav-link" to="/">홈</router-link>
       </li>
-      <li>
+      <li class="nav-li">
         <router-link class="nav-link" to="/products">상품목록</router-link>
       </li>
-      <li>
+      <li class="nav-li">
         <router-link class="nav-link" to="/cart">장바구니</router-link>
       </li>
     </ul>
