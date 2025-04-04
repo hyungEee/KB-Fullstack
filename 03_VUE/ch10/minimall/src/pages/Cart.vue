05a18826218f867fa3bc32127b01dc24903eb48a
지형 <97657166+hyungEee@users.noreply.github.com>
2025-04-04 21:27:48 +0900

    Update Cart.vue

--- 03_VUE/ch10/minimall/src/pages/Cart.vue
+++ 03_VUE/ch10/minimall/src/pages/Cart.vue
@@ -1,11 +1,55 @@
+<style>
+.cart-item-li {
+  list-style: none;
+  display: flex;
+  flex-direction: row;
+  border: 1px solid gray;
+  border-radius: 15px;
+  padding: 10px;
+  margin: 10px;
+}
+.cart-item-info {
+  display: flex;
+  flex-direction: column;
+  padding: 20px;
+}
+.cart-img {
+  width: 150px;
+}
+.total {
+  text-align: center;
+  font-size: 1.2em;
+}
+</style>
 <template>
   <div>
-    <h2>장바구니 페이지입니다.</h2>
+    <h2>🛒 장바구니</h2>
+    <br />
+    <ul style="padding-left: 0">
+      <li v-for="(item, index) in cart" :key="index" class="cart-item-li">
+        <img :src="item.image" class="cart-img" />
+        <div class="cart-item-info">
+          <div style="font-weight: bold">{{ item.name }}</div>
+          <div style="margin-top: 10px">{{ item.price }}원</div>
+        </div>
+      </li>
+    </ul>
+    <br />
+    <div class="total">총합: {{ total }}원</div>
   </div>
 </template>
 
 <script>
+import { inject } from "vue";
+
 export default {
   name: "cart",
+  setup() {
+    const cart = inject("cart");
+    const total = cart.reduce((acc, cur) => {
+      return acc + cur.price;
+    }, 0);
+    return { cart, total };
+  },
 };
 </script>
